8457f237e88d03150652e748b268b513d212a5ca
leticiafrontend <leticia.alesilva@outlook.com>
2021-11-12 08:59:11 -0300

    feature: add fake server

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,9 +1,26 @@
-import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+import api from 'services/api'
+import { IProduct } from '../store/modules/cart/types'
 
 export const Catalog = () => {
-  const catalog = useSelector((state) => state)
+  const [catalog, setCatalog] = useState<IProduct[]>([])
+
+  useEffect(() => {
+    api.get('products').then((response) => setCatalog(response.data))
+  }, [])
 
   console.log(catalog)
 
-  return <h1>Catalog</h1>
+  return (
+    <main>
+      <h1>Catalog</h1>
+      {catalog.map((product) => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {' - '}
+          <span>{product.price}</span> {' - '}
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
+  )
 }
